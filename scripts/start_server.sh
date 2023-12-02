2e3c83cad092b29b002058f82268e6de8ad62a0d
sammy-uno <mustaphafriha@gmail.com>
2023-12-02 03:50:52 -0600

    new changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,5 @@ source book-api/bin/activate
 cd /home/ec2-user/Books-RDF
 export API_DEBUG=False
 ./startServer.sh &> Books-DRF.log &
-ps -ef | grep waitres
+echo "Hello"
+echo (ps -ef | grep waitres)
